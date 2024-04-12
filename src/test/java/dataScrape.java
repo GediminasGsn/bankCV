7ba2a9887389dd7273d84eeda9d2645aa70c6b3c
GediminasGsn <gasiunasgediminas@gmail.com>
2024-04-12 17:08:01 +0300

    Full

--- src/test/java/dataScrape.java
+++ src/test/java/dataScrape.java
@@ -27,7 +27,6 @@ public class dataScrape {
         //accept cookies
         driver.findElement(By.xpath("/html/body/div[18]/div[2]/div[1]/div[2]/div[2]/button[1]")).click();
         //Loop through every page
-
         for (int i = 1; i < 201; i++) {
 
             String url1 = "https://www.cvbankas.lt/?location%5B0%5D=606&padalinys%5B0%5D=76&page=" + i;
@@ -35,18 +34,22 @@ public class dataScrape {
             if (!driver.getCurrentUrl().equals(url1)) {
                 break; //end loop
             }
-
+            //Wait
             try {
                 Thread.sleep(5000); // Wait for 5 seconds
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-
+            //Check all adds
             List<WebElement> h3Elements = driver.findElements(By.xpath("//article"));
-
+            List<WebElement> h3Position = driver.findElements(By.xpath("//h3"));
+            List<WebElement> h3Salary = driver.findElements(By.xpath("//*[@id=\"job_ad_10241525\"]/a/div[2]/div[2]/span/span"));
+            //Run it through condition
             for (WebElement h3 : h3Elements) {
                 String h3Text = h3.getText();
                 if (h3Text.contains("QA") || h3Text.contains("automation") || h3Text.contains("engineer") || h3Text.contains("testuotojas")) {
+                    System.out.println("");
+                    System.out.println(h3.getText());
                     System.out.println("Skelbimo URL: " + h3.findElement(By.xpath(".//a")).getAttribute("href"));
                 }
 
