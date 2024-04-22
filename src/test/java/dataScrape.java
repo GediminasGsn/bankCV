0d4803858f5775e89a0d84c08fa2c543bbd34969
GediminasGsn <gasiunasgediminas@gmail.com>
2024-04-22 18:50:07 +0300

    final

--- src/test/java/dataScrape.java
+++ src/test/java/dataScrape.java
@@ -61,7 +61,7 @@ public class dataScrape {
             //Run it through condition
             for (WebElement h3 : h3Elements) {
                 String h3Text = h3.getText();
-                if (h3Text.contains("QA") || h3Text.contains("automation") || h3Text.contains("engineer") || h3Text.contains("testuotojas") || h3Text.contains("Junior") || h3Text.contains("Tester") || h3Text.contains("Quality") || h3Text.contains("Assurance")) {
+                if (h3Text.contains("QA") || h3Text.contains("automation") || h3Text.contains("engineer") || h3Text.contains("testuotojas") || h3Text.contains("Junior") || h3Text.contains("Tester") || h3Text.contains("Quality") || h3Text.contains("Assurance") || h3Text.contains("Manual") || h3Text.contains("Rankinis")) {
                     System.out.println("");
                     System.out.println("Pozicija: " + h3.findElement(By.xpath(".//h3")).getText()); // Print position
 //                    System.out.println("Darbo uždarbis: " + h3.findElement(By.xpath("/html/body/div[1]/div/div/main/div/article[1]/a/div[2]/div[2]/span/span")).getText()); //Print salary
